9537bf17e69ebb08ceaf59fdc790dec5e603e99d
Vance Spencer <vancespencer@Vances-MacBook-Pro.local>
2019-04-10 08:36:39 -0700

    charts working, starting to code out datafeeds crud

--- src/pages/CoinPage/CoinPage.jsx
+++ src/pages/CoinPage/CoinPage.jsx
@@ -1,31 +1,14 @@
 import React, {Component} from 'react'
-import coinService from '../../utils/coinService'
 import Chart from '../../components/Chart/Chart'
 
 
 class CoinPage extends Component {
-                                                                                                                                                                                         
-    state = {
-        prices: []
-    }
-
-    async componentDidMount() {
-        try {
-            await coinService.getOneCoin(this.props.match.params.id)
-            .then(data => {
-                this.setState({prices: data})
-                console.log(this.state.prices)
-        })} catch (err) {
-            console.log('ERR', err)
-        }
-    }
     
     render() {
         return (
         <>
         <Chart 
         name={this.props.match.params.id}
-        prices={this.state.prices}
         />
         </>
     )}
